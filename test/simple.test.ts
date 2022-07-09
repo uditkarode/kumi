3ea881b9d04384d1d327ff2965ce15d78b24266b
Udit Karode <udit.karode@gmail.com>
2022-07-09 23:08:41 +0530

    feat(test): add markdown test

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,15 +1,13 @@
 import { spacesBetween, stringl } from "../src/combinators";
 import { Parser } from "../src/parser";
 
-describe("Simple Tests", () => {
-  it("should parse with spacesBetween", () => {
-    const parser = new Parser();
+it("should parse with spacesBetween", () => {
+  const parser = new Parser();
 
-    const result = parser.parse(
-      "hi hello",
-      spacesBetween([stringl("hi"), stringl("hello")])
-    );
+  const result = parser.parse(
+    "hi hello",
+    spacesBetween([stringl("hi"), stringl("hello")])
+  );
 
-    expect(result).toEqual(["hi", "hello"]);
-  });
+  expect(result).toEqual(["hi", "hello"]);
 });
