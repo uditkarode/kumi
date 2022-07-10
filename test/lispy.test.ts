dd037ac0dd6d635ef71f1dbc9639b53b7631f4b3
Udit Karode <udit.karode@gmail.com>
2022-07-10 14:38:34 +0530

    cleanup(test/lispy): better namings

--- test/lispy.test.ts
+++ test/lispy.test.ts
@@ -13,50 +13,47 @@ import { Parser } from "../src/parser";
 import { Combinator } from "../src/types";
 import { inspect } from "util";
 
-const son = Parser.combinator((ca) => {
-  many(oneOf(stringl("\n"), oneOf(stringl(" "), stringl("\t"))))(ca);
+const whitespace = Parser.combinator((ca) => {
+  return many(oneIn(stringl("\n"), stringl(" "), stringl("\t")))(ca);
 });
 
+const peek: Combinator<string> = (ca) => ca.target[ca.cursor.get()];
+
 it("should be able to parse a simple lispy structure", () => {
-  type Content = string | [string, Content] | Content[];
+  type FunContent = string | [string, FunContent] | FunContent[];
 
-  const string = Parser.combinator((ca) => {
-    return Parser.expect(within('"', '"')(ca));
-  });
-
-  const inner: Combinator<Content> = (ca) => {
-    // first is the function name
+  const funContent: Combinator<FunContent> = (ca) => {
     // like h1 in (h1 "hi")
-    const funName = firstIn([until(" "), until(")"), until("\n"), until("\t")])(
-      ca
-    );
+    const funName = oneIn(until(" "), until(")"), until("\n"), until("\t"))(ca);
 
-    // hack to peek atm
     // we return early here if the function ends here (i.e. nothing inside)
     // like (h1)
-    if (ca.target[ca.cursor.get()] == ")") {
-      return [funName];
-    }
+    if (peek(ca) == ")") return [funName];
 
-    // spaces or tabs after the function name
+    // whitespace after the function name
     // (h1 "hi")
     //    ^
-    son(ca);
+    whitespace(ca);
+
+    const word: Combinator<string> = (ca) =>
+      // Parser.expect throws on failure
+      Parser.expect(within('"', '"')(ca));
 
     // either many other functions or just a string can be inside
-    return [funName, oneIn<Content>(many(fun, false), string)(ca)];
+    return [funName, oneIn<FunContent>(many(fun, false), word)(ca)];
   };
 
   const fun = Parser.combinator((ca) => {
-    const content = combinatorWithin("(", inner, ")")(ca);
+    const content = combinatorWithin("(", funContent, ")")(ca);
     // whitespace that might be after the content ends
-    son(ca);
     return content;
   });
 
-  const lispy = Parser.combinator((ca) => {
-    son(ca);
-    return many(fun, false)(ca);
+  const lispyStructure = Parser.combinator((ca) => {
+    whitespace(ca);
+    const r = many(fun, false)(ca);
+    whitespace(ca);
+    return r;
   });
 
   // parser runner code
@@ -65,12 +62,12 @@ it("should be able to parse a simple lispy structure", () => {
     `
 (html
   (h1 "hello")
-  (p "what's up)
+  (p "what's up")
   (list
     (ul (p "hi"))
     (ul (p "bye")))
   (hi))`,
-    lispy
+    lispyStructure
   );
 
   if (result instanceof ParseError) {
